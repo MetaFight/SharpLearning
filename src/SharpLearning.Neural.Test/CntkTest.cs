9787ed606efa48444200fa7a759476776e88e2e3
mdabros <mdabros@users.noreply.github.com>
2017-09-19 20:47:32 +0200

    Add Cntk example using f64matrix as input [WIP]

---
+++ src/SharpLearning.Neural.Test/CntkTest.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using CNTK;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpLearning.Containers.Matrices;
+using SharpLearning.Containers.Extensions;
+using SharpLearning.InputOutput.Csv;
+
+namespace SharpLearning.Neural.Test.Cntk
+{
+    [TestClass]
+    public class CntkTest
+    {
+        //[TestMethod]
+        public void Learn_CNN_Using_F64Matrix_As_Source()
+        {
+            // read data
+            var targetName = "Class";
+            var parser = new CsvParser(() => new StreamReader(@"mnist location"));
+            var observations = parser.EnumerateRows(r => r != targetName).ToF64Matrix();
+            observations.Map(v => v / 256.0); // scale input between 0 and 1.
+
+            var targets = parser.EnumerateRows(targetName).ToF64Vector();
+            var encodedTargets = Encode(targets);
+            var learningIndices = Enumerable.Range(0, targets.Length).ToArray();
+            
+            var inputDim = new int[] { 28, 28, 1 };
+            int numberOfClasses = encodedTargets.ColumnCount;
+
+            var featureVariable = Variable.InputVariable(inputDim, DataType.Float);
+            var targetVariable = Variable.InputVariable(new int[] { numberOfClasses }, DataType.Float);
+
+            // setup network
+            var network = CreateNetwork(featureVariable, numberOfClasses);
+            var loss = CNTKLib.CrossEntropyWithSoftmax(network, targetVariable);
+            var evalError = CNTKLib.ClassificationError(network, targetVariable);
+                       
+            var device = CntkLayers.Device;
+            Trace.WriteLine($"Using device: {device.Type}");
+
+            // setup learner
+            int minibatchSize = 128;
+            var epochs = 25;
+            var learningRatePerSample = new CNTK.TrainingParameterScheduleDouble(0.0003125, 1);
+            var parameterLearners = new List<Learner>() { Learner.SGDLearner(network.Parameters(), learningRatePerSample) };
+            var trainer = Trainer.CreateTrainer(network, loss, evalError, parameterLearners);
+
+            // train the model
+            var batchContainer = new Dictionary<Variable, Value>();
+            var random = new Random(23);
+            var numberOfBatches = targets.Length / minibatchSize;
+
+            for (int epoch = 0; epoch < epochs; epoch++)
+            {
+                var accumulatedLoss = 0.0;
+                learningIndices.Shuffle(random);
+
+                for (int i = 0; i < numberOfBatches; i++)
+                {
+                    var workIndices = learningIndices
+                        .Skip(i * minibatchSize)
+                        .Take(minibatchSize).ToArray();
+
+                    if (workIndices.Length != minibatchSize)
+                    {
+                        continue; // only train with full batch size
+                    }
+
+                    var features = CopyBatch(observations, workIndices);
+                    var batchLabels = CopyBatch(encodedTargets, workIndices);
+
+                    using (var batchObservations = Value.CreateBatch<float>(inputDim, features, device))
+                    using (var batchTarget = Value.CreateBatch<float>(new int[] { numberOfClasses }, batchLabels, device))
+                    {
+                        batchContainer.Add(featureVariable, batchObservations);
+                        batchContainer.Add(targetVariable, batchTarget);
+
+                        trainer.TrainMinibatch(batchContainer, device);
+                        batchContainer.Clear();
+
+                        accumulatedLoss += trainer.PreviousMinibatchSampleCount() * trainer.PreviousMinibatchLossAverage();
+                    }
+                }
+                
+                var currentLoss = accumulatedLoss / (double)targets.Length;
+                Trace.WriteLine($"Epoch: {epoch + 1}: Loss = {currentLoss}");
+            }
+        }
+
+        static Function CreateNetwork(Variable input, int numberOfClasses)
+        {
+            var network = CntkLayers.Conv2D(input, 5, 5, 32);
+            network = CntkLayers.Activation(network, Activation.ReLU);
+            network = CntkLayers.Pool2D(network, 2, 2);
+
+            network = CntkLayers.Conv2D(network, 5, 5, 32);
+            network = CntkLayers.Activation(network, Activation.ReLU);
+            network = CntkLayers.Pool2D(network, 2, 2);
+
+            network = CntkLayers.Dense(network, 256);
+            network = CntkLayers.Activation(network, Activation.ReLU);
+            network = CntkLayers.Dense(network, numberOfClasses);
+            return network;
+        }
+
+        public static float[] CopyBatch(F64Matrix observations, int[] indices)
+        {
+            var batch = observations.Rows(indices);
+            return batch.Data().Select(v => (float)v).ToArray();
+        }
+
+        public static F64Matrix Encode(double[] targets)
+        {
+            var index = 0;
+            var targetNameToTargetIndex = targets.Distinct().OrderBy(v => v)
+                .ToDictionary(v => v, v => index++);
+
+            var oneOfN = new F64Matrix(targets.Length, targetNameToTargetIndex.Count);
+
+            for (int i = 0; i < targets.Length; i++)
+            {
+                var target = targets[i];
+                oneOfN[i, targetNameToTargetIndex[target]] = 1.0f;
+            }
+
+            return oneOfN;
+        }
+    }
+}
